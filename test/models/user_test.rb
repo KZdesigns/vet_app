16d6150b66e92821c55035d07666e23e65586dc5
Kyle Zimmerman <kzdesignsios@gmail.com>
2023-02-15 13:49:59 -0800

    Finished Model Validation test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,7 +10,7 @@ class UserTest < ActiveSupport::TestCase
     user6 = User.new(email: "@test.com", password: "password", phone: "7779992222")
     user7 = User.new(email: "test@.com", password: "password", phone: "7779992222")
     
-    assert user1.save
+    assert user1.save #happy path
     assert_not user2.save #check for uniqueness
     assert_not user3.save #check for presence
     assert_not user4.save #check for format
@@ -18,10 +18,24 @@ class UserTest < ActiveSupport::TestCase
     assert_not user6.save #check for format
     assert_not user7.save #check for format
   end
-  #TODO: finish writing test for validations on User Model
+
   test "password validation" do
+    user1 = User.new(email: "test1@test.com", password: "password", phone: "7779992222")
+    user2 = User.new(email: "test2@test.com", password: "", phone: "7779992222")
+    user3 = User.new(email: "test3test.com", password: "pass", phone: "7779992222")
+
+    assert user1.save #test happy path
+    assert_not user2.save #test presence
+    assert_not user3.save #test password length
   end
 
   test "phone number validation" do
+    user1 = User.new(email: "test1@test.com", password: "password", phone: "7779992222")
+    user2 = User.new(email: "test2@test.com", password: "password", phone: "")
+    user3 = User.new(email: "test3test.com", password: "password", phone: "777999222")
+
+    assert user1.save #happy path
+    assert_not user2.save #check for presence
+    assert_not user3.save #check for length
   end
 end
