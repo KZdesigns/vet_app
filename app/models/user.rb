56df9d52b71f89745081e090c79ce2de129185d8
Kyle Zimmerman <kzdesignsios@gmail.com>
2023-02-14 22:24:22 -0800

    WOP writing tests for user model
    added to TODO commit in user_test file
    
    WOP need to finsih user model testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :email, uniqueness: { message: "must be a unique email"}
-    validates :email, :password, :phone, presence: { message: "must have a password and phone number"}
+    validates :email, :password, :phone, presence: { message: "must have an email, password, and a phone number"}
     validates :phone, length: { minimum: 10 }
     validates :password, length: { minimum: 5 }
-    validates :password, format: { with: /\A[\w.+-]+@\w+\.\w+\z/ } #TODO: learn more about rejects delete this comment when spike is complete
+    validates :email, format: { with: /\A[\w.+-]+@\w+\.\w+\z/ } #TODO: learn more about regex delete this comment when spike is complete
 end
