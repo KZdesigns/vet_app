9e8fe8df1a85d9a76f1190e980647861ff079b6a
Kyle Zimmerman <kzdesignsios@gmail.com>
2023-02-14 21:41:56 -0800

    added validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :email, uniqueness: { message: "must be a unique email"}
+    validates :email, :password, :phone, presence: { message: "must have a password and phone number"}
+    validates :phone, length: { minimum: 10 }
+    validates :password, length: { minimum: 5 }
+    validates :password, format: { with: /\A[\w.+-]+@\w+\.\w+\z/ } #TODO: learn more about rejects delete this comment when spike is complete
 end
